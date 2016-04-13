91d88dab2590f1ae26925ac40f5919398d25619d
Erick Maia <Erick Maia@192.168.0.27>
2016-04-13 15:06:29 -0300

    Adicionado os testes para endereco e outras modificacoes
    Signed-off-by: Erick Maia <Erick Maia@192.168.0.27>

--- src/main/java/br/com/contmatic/entity/empresa/Endereco.java
+++ src/main/java/br/com/contmatic/entity/empresa/Endereco.java
@@ -5,19 +5,30 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Endereco {
+	private static final int TAMANHO_MAX_NOME_LOGRADOURO = 30;
+	private static final int TAMANHO_MIN_NOME_LOGRADOURO = 4;
+	private static final int TAMANHO_MAX_TIPO_LOGRADOURO = 15;
+	private static final int TAMANHO_MIN_TIPO_LOGRADOURO = 2;
+	private static final int TAMANHO_TELEFONE = 9;
+	private static final int TAMANHO_CEP = 8;
+	private static final int TAMANHO_MAX_CIDADE = 30;
+	private static final int TAMANHO_MAX_ESTADO = 15;
+	private static final int TAMANHO_MAX_BAIRRO = 15;
 	private String nomeLogradouro;
 	private String tipoLogradouro;
-	private int numero;
+	private String numero;
 	private String cep;
 	private String cidade;
 	private String estado;
 	private String bairro;
+
 
 	public String getNomeLogradouro() {
 		return nomeLogradouro;
 	}
 
 	public void setNomeLogradouro(String nomeLogradouro) {
+		this.validateNomeLogradouroAll(nomeLogradouro);
 		this.nomeLogradouro = nomeLogradouro;
 	}
 
@@ -26,14 +37,16 @@ public class Endereco {
 	}
 
 	public void setTipoLogradouro(String tipoLogradouro) {
+		this.validateTipoLogradouroAll(tipoLogradouro);
 		this.tipoLogradouro = tipoLogradouro;
 	}
 
-	public int getNumero() {
+	public String getNumero() {
 		return numero;
 	}
 
-	public void setNumero(int numero) {
+	public void setNumero(String numero) {
+		this.validateNumeroAll(numero);
 		this.numero = numero;
 	}
 
@@ -42,6 +55,7 @@ public class Endereco {
 	}
 
 	public void setCep(String cep) {
+		this.validateCepAll(cep);
 		this.cep = cep;
 	}
 
@@ -50,6 +64,7 @@ public class Endereco {
 	}
 
 	public void setCidade(String cidade) {
+		this.validateCidadeAll(cidade);
 		this.cidade = cidade;
 	}
 
@@ -58,6 +73,7 @@ public class Endereco {
 	}
 
 	public void setEstado(String estado) {
+		this.validateEstadoAll(estado);
 		this.estado = estado;
 	}
 
@@ -66,9 +82,215 @@ public class Endereco {
 	}
 
 	public void setBairro(String bairro) {
+		this.validateBairroAll(bairro);
 		this.bairro = bairro;
 	}
 
+	private void validateNomeLogradouroNotNull(String nomeLogradouro){
+		if(nomeLogradouro==null){
+			throw new IllegalArgumentException("Nome Logradouro não pode ser nulo");
+		}
+	}
+	
+	private void validateNomeLogradouroVazio(String nomeLogradouro){
+		if(nomeLogradouro.isEmpty()){
+			throw new IllegalArgumentException("Nome Logradouro não pode estar vazio");
+		}
+	}
+	
+	private void validateNomeLogradouroTamanhoMaximo(String nomeLogradouro){
+		if(nomeLogradouro.length()>TAMANHO_MAX_NOME_LOGRADOURO){
+			throw new IllegalArgumentException("Tamanho Maximo do logradouro é: "+TAMANHO_MAX_NOME_LOGRADOURO);
+		}
+	}
+	
+	private void validateNomeLogradouroTamanhoMinimo(String nomeLogradrouro){
+		if(nomeLogradrouro.length()<TAMANHO_MIN_NOME_LOGRADOURO){
+			throw new IllegalArgumentException("Tamanho Minimo do logradouro é: "+ TAMANHO_MIN_NOME_LOGRADOURO);
+		}
+	}
+	
+	private void validateNomeLogradouroAll(String nomeLogradouro){
+		this.validateNomeLogradouroNotNull(nomeLogradouro);
+		this.validateNomeLogradouroVazio(nomeLogradouro);
+		this.validateNomeLogradouroTamanhoMinimo(nomeLogradouro);
+		this.validateNomeLogradouroTamanhoMaximo(nomeLogradouro);
+	}
+	
+	private void validateTipoLogradouroNotNull (String tipoLogradouro){
+		if (tipoLogradouro==null){
+			throw new IllegalArgumentException("Tipo do logradouro nao pode ser nulo");
+		}
+	}
+	
+	private void validateTipoLogradouroVazio(String tipoLogradouro){
+		if(tipoLogradouro.isEmpty()){
+			throw new IllegalArgumentException("Tipo do logradouro não pode ser vazio");
+		}
+	}
+	
+	private void validateTipoLogradouroTamanhoMaximo(String tipoLogradouro){
+		if(tipoLogradouro.length()>TAMANHO_MAX_TIPO_LOGRADOURO){
+			throw new IllegalArgumentException("Tamanho Maximo do logradouro é: "+TAMANHO_MAX_TIPO_LOGRADOURO);
+		}
+	}
+	
+	private void validateTipoLogradouroTamanhoMinimo(String tipoLogradouro){
+		if(tipoLogradouro.length()<TAMANHO_MIN_TIPO_LOGRADOURO){
+			throw new IllegalArgumentException("Tamanho Minimo do logradouro é: "+ TAMANHO_MIN_TIPO_LOGRADOURO);
+		}
+	}
+	
+	private void validateTipoLogradouroAll(String tipoLogradouro){
+		this.validateTipoLogradouroNotNull(tipoLogradouro);
+		this.validateTipoLogradouroVazio(tipoLogradouro);
+		this.validateTipoLogradouroTamanhoMinimo(tipoLogradouro);
+		this.validateTipoLogradouroTamanhoMaximo(tipoLogradouro);
+	}
+	
+	private void validateNumeroNotNull (String numero){
+		if (numero==null){
+			throw new IllegalArgumentException("O numero não pode ser nulo");
+		}
+	}
+	
+	private void validateNumeroVazio(String numero){
+		if(numero.isEmpty()){
+			throw new IllegalArgumentException("O numero não pode ser vazio");
+		}
+	}
+	
+	private void validateNumeroTamanhoIncorreto(String numero){
+		if(numero.length()!=TAMANHO_TELEFONE){
+			throw new IllegalArgumentException("O tamanho do telefone esta incorreto, o telefone precisa ter: "+TAMANHO_TELEFONE);
+		}
+	}
+	
+	private void validateNumeroAll(String numero){
+		this.validateNumeroNotNull(numero);
+		this.validateNumeroVazio(numero);
+		this.validateNumeroTamanhoIncorreto(numero);
+	}
+	
+	private void validateCepComLetras(String cep){
+		for (int i = 0; i < cep.length(); i++) {
+			char cepchar=cep.charAt(i);
+			if(Character.isLetter(cepchar)){
+				throw new IllegalArgumentException("O CEP não pode conter letras");
+			}
+		}
+		
+	}
+	
+	private void validateCepNotNull(String cep){
+		if(cep==null){
+			throw new IllegalArgumentException("O CEP não pode ser nulo ");
+		}
+	}
+	
+	private void validateCepVazio(String cep){
+		if(cep.isEmpty()){
+			throw new IllegalArgumentException("O CEP não pode ser avzio");
+		}
+	}
+	
+	private void validateCepTamanhoIncorreto(String cep){
+		if (cep.length()!=TAMANHO_CEP){
+			throw new IllegalArgumentException("O tamanho do CEP esta incorreto, o tamanho do cep é: "+TAMANHO_CEP);
+		}
+	}
+	
+	private void validateCepAll(String cep){
+		this.validateCepNotNull(cep);
+		this.validateCepVazio(cep);
+		this.validateCepComLetras(cep);
+		this.validateCepTamanhoIncorreto(cep);
+	}
+	
+	private void validateCidadeNotNull(String cidade){
+		if(cidade==null){
+			throw new IllegalArgumentException("A cidade não pode ser nulo");
+		}
+	}
+	
+	private void validateCidadeVazio(String cidade){
+		if(cidade.isEmpty()){
+			throw new IllegalArgumentException("A cidade não pode ser vazio");
+		}
+	}
+	
+	private void validateCidadeComNumero(String cidade){
+		for (int i = 0; i < cidade.length(); i++) {
+			char cidadechar = cidade.charAt(i);
+			if(Character.isDigit(cidadechar)){
+				throw new IllegalArgumentException("A cidade não pode conter numeros");
+			}
+		}
+	}
+	
+	private void validateCidadeTamanhoIncorreto(String cidade){
+		if (cidade.length()>TAMANHO_MAX_CIDADE){
+			throw new IllegalArgumentException("O campo cidade não pode conter mais que: "+TAMANHO_MAX_CIDADE+" carateres");
+		}
+	}
+	
+	private void validateCidadeAll(String cidade){
+		this.validateCidadeNotNull(cidade);
+		this.validateCidadeVazio(cidade);
+		this.validateCidadeComNumero(cidade);
+		this.validateCidadeTamanhoIncorreto(cidade);
+	}
+	
+	private void validateEstadoNotNull(String estado){
+		if(estado==null){
+			throw new IllegalArgumentException("O estado não pode ser nulo");
+		}
+	}
+	
+	private void validateEstadoVazio(String estado){
+		if(estado.isEmpty()){
+			throw new IllegalArgumentException("O estado não pode ser vazio");
+		}
+	}
+	
+	private void validateEstadoTamanhoCorreto(String estado){
+		if(estado.length()>TAMANHO_MAX_ESTADO){
+			throw new IllegalArgumentException("O campo estado nao pode conter mais que: "+TAMANHO_MAX_ESTADO+" caracteres");
+		}
+	}
+	
+	private void validateEstadoAll(String estado){
+		this.validateEstadoNotNull(estado);
+		this.validateEstadoVazio(estado);
+		this.validateEstadoTamanhoCorreto(estado);
+	}
+	
+	private void validateBairroNulo(String bairro){
+		if (bairro==null){
+			throw new IllegalArgumentException("O bairro não pode ser nulo");
+		}
+	}
+	
+	private void validateBairroVazio(String bairro){
+		if(bairro.isEmpty()){
+			throw new IllegalArgumentException("O bairro não pode ser vazio");
+		}
+	}
+	
+	private void validateBairroTamanhoIncorreto(String bairro){
+		if(bairro.length()>TAMANHO_MAX_BAIRRO){
+			throw new IllegalArgumentException("O campo bairro nao pode conter mais que: "+TAMANHO_MAX_BAIRRO+" caracteres");
+		}
+	}
+	
+	private void validateBairroAll(String bairro){
+		this.validateBairroNulo(bairro);
+		this.validateBairroVazio(bairro);
+		this.validateBairroTamanhoIncorreto(bairro);
+	}
+	
+	
+	
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder().append(this.cep).append(this.numero).toHashCode();
--- src/test/java/br/com/contmatic/entity/empresa/teste/EmpresaTeste.java
+++ src/test/java/br/com/contmatic/entity/empresa/teste/EmpresaTeste.java
@@ -1,8 +1,6 @@
 package br.com.contmatic.entity.empresa.teste;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.runners.MethodSorters.NAME_ASCENDING;
 
 import org.junit.Before;
@@ -17,187 +15,120 @@ import br.com.contmatic.entity.empresa.Telefone;
 @FixMethodOrder(NAME_ASCENDING)
 
 public class EmpresaTeste {
-	private Empresa p1;
+	private Empresa empresa1;
 	private Endereco endereco;
 	private Telefone tel;
 
 	@Before
 	public void setUp() {
-		p1 = new Empresa();
+		empresa1 = new Empresa();
 		endereco = new Endereco();
-		tel = new Telefone();
-		tel.setDdd("011");
-		tel.setNumeroTelefone("123456789");
-		tel.setTipoTelefone("celular");
 		
-		p1.setCnpj("11123121111111");
-		p1.setEmail("ssssssss@sss.com.br");
-		p1.setEndereco(endereco);
-		p1.setNomeFantasia("nome da empresa");
-		p1.setNomeProprietario("proprietario");
-		p1.setTelefone(tel);
+		
+		empresa1.setCnpj("11123121111111");
+		empresa1.setEmail("ssssssss@sss.com.br");
+		empresa1.setEndereco(endereco);
+		empresa1.setNomeFantasia("nome da empresa");
+		empresa1.setNomeProprietario("proprietario");
+		empresa1.setTelefone(tel);
 
 	}
 
 	@Test
 	public void nao_deve_aceitar_nome_fantasia_nulo() {
-		p1.setNomeFantasia(null);
+		empresa1.setNomeFantasia(null);
 	}
 	
 	@Test
 	public void nao_deve_aceitar_nome_fantasia_vazio(){
-		p1.setNomeFantasia("");
+		empresa1.setNomeFantasia("");
 	}
 	@Test
 	public void nao_deve_aceitar_nome_fantasia_com_minimo_de_caracteres(){
-		p1.setNomeFantasia("aaaa");
+		empresa1.setNomeFantasia("aaaa");
 	}
 	@Test
 	public void nao_deve_aceitar_nome_fantasia_com_maximo_de_caracteres(){
-		p1.setNomeFantasia("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		empresa1.setNomeFantasia("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 	}
 
 	@Test
 	public void nao_deve_aceitar_nome_proprietario_nulo(){
-		p1.setNomeProprietario(null);
+		empresa1.setNomeProprietario(null);
 	}
 	
 	@Test
 	public void nao_deve_aceitar_nome_proprietario_com_numeros(){
-		p1.setNomeProprietario("er111");
+		empresa1.setNomeProprietario("er111");
 	}
 	
 	@Test
 	public void nao_deve_aceitar_nome_proprietario_vazio(){
-		p1.setNomeProprietario("");
+		empresa1.setNomeProprietario("");
 	}
 	
 	@Test
 	public void nao_deve_aceitar_nome_proprietario_com_tamanho_maior(){
-		p1.setNomeProprietario("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+		empresa1.setNomeProprietario("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 	}
 	
 	@Test
 	public void nao_deve_aceitar_nome_proprietario_com_tamanho_menor(){
-		p1.setNomeProprietario("ffff");
+		empresa1.setNomeProprietario("ffff");
 	}
 	
-	@Test
-	public void nao_deve_aceitar_telefone_nulo() {
-		tel.setNumeroTelefone(null);	
-	}
-	
-	@Test
-	public void nao_deve_aceitar_telefone_vazio(){
-		tel.setNumeroTelefone("");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_telefone_tamanho_incorreto(){
-		tel.setNumeroTelefone("11");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_telefone_com_letras(){
-		tel.setNumeroTelefone("fff");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_ddd_nulo(){
-		tel.setDdd(null);
-	}
-	
-	@Test
-	public void nao_deve_aceitar_ddd_vazio(){
-		tel.setDdd("");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_ddd_com_tamanho_errado(){
-		tel.setDdd("44444");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_ddd_com_letras(){
-		tel.setDdd("ddd");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_tipo_telefone_vazio(){
-		tel.setTipoTelefone("");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_tipo_telefone_com_numeros(){
-		tel.setTipoTelefone("2344");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_tipo_telefone_tamanho_maximo(){
-		tel.setTipoTelefone("ffffffffffffffffffffffffffffffffffffff");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_tipo_telefone_tamanho_minimo(){
-		tel.setTipoTelefone("fff");
-	}
-	
-	@Test
-	public void nao_deve_aceitar_tipo_telefone_nulo(){
-		tel.setTipoTelefone(null);
-	}
 	
 	@Test
 	public void nao_deve_aceitar_cnpj_nulo() {
-		p1.setCnpj(null);
+		empresa1.setCnpj(null);
 	}
 
 	@Test
 	public void nao_deve_aceitar_cnpj_com_letras(){
-		p1.setCnpj("asdfas");
+		empresa1.setCnpj("asdfas");
 	}
 	@Test
 	public void nao_deve_aceitar_cnpj_vazio(){
-		p1.setCnpj("");
+		empresa1.setCnpj("");
 	}
 	@Test
 	public void nao_deve_aceitar_cnpj_com_tamanho_incorreto(){
-		p1.setCnpj("1111");
+		empresa1.setCnpj("1111");
 	}
 	@Test
 	public void nao_deve_aceitar_nomeproprietario_nulo() {
-		assertNotNull(p1.getNomeProprietario());
+		assertNotNull(empresa1.getNomeProprietario());
 	}
 	@Ignore
 	@Test
 	public void nao_deve_aceitar_data_de_criacao_nulo() {
-		assertNotNull(p1.getCriacao());
+		assertNotNull(empresa1.getCriacao());
 	}
 
 	@Test
 	public void nao_deve_aceitar_email_nulo() {
-		p1.setEmail(null);
+		empresa1.setEmail(null);
 	}
 	
 	@Test
 	public void nao_deve_aceitar_email_vazio(){
-		p1.setEmail("");
+		empresa1.setEmail("");
 	}
 	
 	@Test
 	public void nao_deve_aceitar_email_tamanho_maximo(){
-		p1.setEmail("fffffffffffffffffffffffffffffffffffffff");
+		empresa1.setEmail("fffffffffffffffffffffffffffffffffffffff");
 	}
 	
 	@Test
 	public void nao_deve_aceitar_email_tamanho_minimo(){
-		p1.setEmail("fff");
+		empresa1.setEmail("fff");
 	}
 
 	
 	@Test
 	public void nao_deve_aceitar_endereco_nulo() {
-		assertNotNull(p1.getEndereco());
+		assertNotNull(empresa1.getEndereco());
 	}
 			
 }
